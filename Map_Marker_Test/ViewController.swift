6382299a3b26366d1257cdac420660fa9b32fdf0
Michael Christie <michaelchristie@Michaels-MacBook-Pro.local>
2020-01-29 20:22:21 +0000

    third

--- Map_Marker_Test/ViewController.swift
+++ Map_Marker_Test/ViewController.swift
@@ -11,19 +11,18 @@ import Firebase
 class ViewController: UIViewController {
     
     var locationsList: [GeoPoint] = []
-    var locationsNames: [String] = []
     var counter = 0
+    
     
     //arrays of names and descriptions
     var names:[String] = []
     var coordinates: [GeoPoint] = []
-    
+    var images: [UIImage] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadDataFromFirebase()
- 
-        //secondfunction()
+        
     }
     
     func loadDataFromFirebase() {
@@ -42,48 +41,29 @@ class ViewController: UIViewController {
                     let coordinate = document.get("Coordinates") as! GeoPoint
                     
                     self.names.append(name)
+                    
                     self.coordinates.append(coordinate)
                     self.locationsList.append(coordinate)
                 }
                 self.secondfunction()
                 
-                for x in self.coordinates{
-                    print()
-                    print(x.latitude)
-                    print()
-                }
+                
             }
         }
     }
     
-    func secondfunction() {
+    func thirdFunction()
+    {
         // Do Stuff
         // Method for displaying map view
         let mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        /*
-         house coords
-         54.25875996736305
-         -8.458672640917941
-         */
         
         mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 54.2710154, longitude: -8.4715214)
         mapView.zoomLevel = 14
         mapView.delegate = self
         view.addSubview(mapView)
         
-        // Specify coordinates for our annotations.
-        /*locationsList = [
-            
-            CLLocationCoordinate2D(latitude: 54.2725555, longitude: -8.4716481),//lillies
-            
-            CLLocationCoordinate2D(latitude: 54.2710154, longitude: -8.4715214),//belfry
-            
-            CLLocationCoordinate2D(latitude: 54.27135042736012, longitude: -8.475986924167955),//penneys
-            
-            //CLLocationCoordinate2D(latitude: 54.25875996736305, longitude: -8.458672640917941)//house
-        ]*/
-
         var pointAnnotations = [MGLPointAnnotation]()
         for coordinate in locationsList {
             let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
@@ -96,28 +76,42 @@ class ViewController: UIViewController {
         mapView.addAnnotations(pointAnnotations)
     }
     
+    func secondfunction() {
+        //https://mapmarkersspace.s3-eu-west-1.amazonaws.com/belfry.png
+        
+        for x in names{
+            let url1 = URL(string: "https://mapmarkersspace.s3-eu-west-1.amazonaws.com/\(x).jpg")
+            print("URL: ", url1)
+            let data1 = try? Data(contentsOf: url1!) //make sure your image in this url does exist
+            //self.imagesOne = UIImage(data: data1!)
+            self.images.append(UIImage(data: data1!)!)
+        }
+        
+        self.thirdFunction()
+    }
+    
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
     }
     
     func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
-        print("TOuched")
         //goToNext
         self.performSegue(withIdentifier: "goToNext", sender: self)
     }
-    
-    
 }
 
 extension ViewController: MGLMapViewDelegate {
+    
     // MGLMapViewDelegate method for adding static images to point annotations
     func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? {
-      
-        print("Extension: ",counter)
         
-        print("Printed: ",names[counter])
+        
         let annotationImage: MGLAnnotationImage
         let annotationImageCocktail = mapView.dequeueReusableAnnotationImage(withIdentifier: names[counter])
+        
+        print()
+        print("HERE: ",self.names[counter])
+        print()
         
         annotationImage = annotationImageCocktail ?? MGLAnnotationImage(image: UIImage(named: names[counter])!, reuseIdentifier: names[counter])
         
